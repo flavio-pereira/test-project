d5001f4d21a2f8c679c8408d2741c36fa6ee9bf1
Flavio Pereira <flaviopereira@g.harvard.edu>
2014-11-02 17:12:58 -0700

    First commit

---
+++ app/routes.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('/get-environment',function() {
+
+    echo "Environment: ".App::environment();
+
+});
+
+Route::get('/trigger-error',function() {
+
+    # Class Foobar should not exist, so this should create an error
+    $foo = new Foobar;
+
+});
